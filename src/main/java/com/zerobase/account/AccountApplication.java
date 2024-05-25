28f305479f5737cf59a81e910c6b8235999455aa
Leegeonmin <kmlee97s@gmail.com>
2024-05-26 00:14:16 +0900

    refactor : JpaEnableJpaAuditing config 분리

--- src/main/java/com/zerobase/account/AccountApplication.java
+++ src/main/java/com/zerobase/account/AccountApplication.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableTransactionManagement
 public class AccountApplication {
 
     public static void main(String[] args) {
